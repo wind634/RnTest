f409f4e80b984cd29896f378a6208b931ef6f290
wind634 <2911184332@qq.com>
2017-06-09 14:54:25 +0800

    ds

--- index.ios.js
+++ index.ios.js
@@ -4,38 +4,98 @@
  * @flow
  */
 
-import React, { Component,Image } from 'react';
+import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+    TextInput,
+  View,
+    Image,
+    ScrollView,
+    FlatList,
+    SectionList,
+Button
 } from 'react-native';
+import {
+    StackNavigator,
+} from 'react-navigation';
 
 
 class RnIosTest extends Component {
-  render() {
-    return (
-        <View>
-          <Text style={styles.red}>just red</Text>
-          <Text style={styles.bigblue}>just bigblue</Text>
-          <Text style={[styles.bigblue, styles.red]}>bigblue, then red</Text>
-          <Text style={[styles.red, styles.bigblue]}>red, then bigblue</Text>
-        </View>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <SectionList
+                    sections={[
+            {title: 'D', data: ['Devin']},
+            {title: 'J', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie']},
+          ]}
+                    renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
+                    renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+                />
+            </View>
+        );
+    }
+
 }
 
-const styles = StyleSheet.create({
-  bigblue: {
-    color: 'blue',
-    fontWeight: 'bold',
-    fontSize: 30,
-  },
-  red: {
-    color: 'red',
-  },
+class MainScreen extends React.Component {
+    static navigationOptions = {
+        title: 'Welcome',
+    };
+    render() {
+        const { navigate } = this.props.navigation;
+        return (
+            <Button
+                title="Go to Jane's profile"
+                onPress={() => navigate('Profile', { name: 'Jane' })
+        }
+            />
+        );
+    }
+}
+
+class ProfileScreen extends Component {
+
+    render() {
+        return (<View>
+            <Text>Hello world</Text>
+        </View>)
+    }
+}
+
+
+const App = StackNavigator({
+    Main: {screen: MainScreen},
+    Profile: {screen: ProfileScreen},
 });
 
 
-AppRegistry.registerComponent('RnIosTest', () => RnIosTest);
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: 22
+    },
+    sectionHeader: {
+        paddingTop: 2,
+        paddingLeft: 10,
+        paddingRight: 10,
+        paddingBottom: 2,
+        fontSize: 14,
+        fontWeight: 'bold',
+        backgroundColor: 'rgba(247,247,247,1.0)',
+    },
+    item: {
+        padding: 10,
+        fontSize: 18,
+        height: 44,
+    },
+})
+
+AppRegistry.registerComponent('RnIosTest', () => App);
